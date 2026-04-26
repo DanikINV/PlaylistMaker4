f6ecffb6816f2ed8eaca2c89841172628fcafd5d
DanikINV <Daniil16200@yandex.ru>
2026-04-26 21:54:11 +0300

    Home Work

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,9 +3,11 @@ package com.example.playlistmaker
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
@@ -14,10 +16,19 @@ import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
     private var searchText: String = ""
+    private lateinit var iTunesService: ITunesApi
+    private val tracks = ArrayList<Track>()
+    private lateinit var adapter: TrackAdapter
+    private lateinit var placeholderMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +37,8 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
 
         val isNightMode = resources.configuration.uiMode and
-            android.content.res.Configuration.UI_MODE_NIGHT_MASK ==
-            android.content.res.Configuration.UI_MODE_NIGHT_YES
+                android.content.res.Configuration.UI_MODE_NIGHT_MASK ==
+                android.content.res.Configuration.UI_MODE_NIGHT_YES
         WindowInsetsControllerCompat(window, window.decorView).apply {
             isAppearanceLightStatusBars = !isNightMode
         }
@@ -43,15 +54,25 @@ class SearchActivity : AppCompatActivity() {
             insets
         }
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        iTunesService = retrofit.create(ITunesApi::class.java)
+
         val btnBack = findViewById<ImageView>(R.id.btn_back)
         btnBack.setOnClickListener { finish() }
 
         val etSearch = findViewById<EditText>(R.id.et_search)
         val btnClear = findViewById<ImageView>(R.id.btn_clear_search)
+        placeholderMessage = findViewById(R.id.placeholder_message)
 
         btnClear.setOnClickListener {
             etSearch.text.clear()
             hideKeyboard(etSearch)
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            placeholderMessage.visibility = View.GONE
         }
 
         etSearch.doOnTextChanged { text, _, _, _ ->
@@ -59,22 +80,52 @@ class SearchActivity : AppCompatActivity() {
             searchText = text?.toString() ?: ""
         }
 
-        val tracks = ArrayList<Track>()
-        tracks.add(Track("Smells Like Teen Spirit", "Nirvana", "5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"))
-        tracks.add(Track("Billie Jean", "Michael Jackson", "4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"))
-        tracks.add(Track("Stayin' Alive", "Bee Gees", "4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"))
-        tracks.add(Track("Whole Lotta Love", "Led Zeppelin", "5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"))
-        tracks.add(Track("Sweet Child O'Mine", "Guns N' Roses", "5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
-
         val rvTracks = findViewById<RecyclerView>(R.id.rv_tracks)
         rvTracks.layoutManager = LinearLayoutManager(this)
-        rvTracks.adapter = TrackAdapter(tracks)
+        adapter = TrackAdapter(tracks)
+        rvTracks.adapter = adapter
 
+        etSearch.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (etSearch.text.isNotEmpty()) {
+                    search(etSearch.text.toString())
+                }
+                true
+            }
+            false
+        }
+    }
+
+    private fun search(query: String) {
+        iTunesService.search(query).enqueue(object : Callback<TracksResponse> {
+            override fun onResponse(call: Call<TracksResponse>, response: Response<TracksResponse>) {
+                if (response.code() == 200) {
+                    tracks.clear()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        tracks.addAll(response.body()?.results!!)
+                        adapter.notifyDataSetChanged()
+                    }
+                    if (tracks.isEmpty()) {
+                        showMessage(getString(R.string.nothing_found))
+                    } else {
+                        placeholderMessage.visibility = View.GONE
+                    }
+                } else {
+                    showMessage(getString(R.string.something_went_wrong))
+                }
+            }
+
+            override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                showMessage(getString(R.string.something_went_wrong))
+            }
+        })
+    }
+
+    private fun showMessage(text: String) {
+        placeholderMessage.visibility = View.VISIBLE
+        tracks.clear()
+        adapter.notifyDataSetChanged()
+        placeholderMessage.text = text
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
